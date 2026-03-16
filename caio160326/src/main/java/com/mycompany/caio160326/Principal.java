9707e63e34fd9706a79925de93aba7ab749df725
CaioZouain-creator <goltarazouaincaio@gmail.com>
2026-03-16 20:31:00 -0300

    Add files via upload

---
+++ caio160326/src/main/java/com/mycompany/caio160326/Principal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.caio160326;
+
+/**
+ *
+ * @author alunolab13
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        
+        Curso c1 = new Curso("oi", "medicina");
+        Aluno al = new Aluno("caio", 123, c1);
+        
+       
+    }
+}
